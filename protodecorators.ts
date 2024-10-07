8ed9bfb8752a12760873e91cf19b358e2ee5a0aa
Denis Murphy <denismurp@gmail.com>
2024-10-07 17:26:44 +0100

    Init commit

--- protodecorators.ts
+++ protodecorators.ts
@@ -15,15 +15,15 @@ interface ProtoContractOptions {
   name?: string;
 }
 
-export function ProtoMember(options: ProtoMemberOptions) {
-  return function(target: any, propertyKey: string) {
+export function ProtoMember(options: ProtoMemberOptions): PropertyDecorator {
+  return function(target: Object, propertyKey: string | symbol) {
     const existingFields = Reflect.getMetadata(PROTO_METADATA_KEY, target) || {};
     existingFields[propertyKey] = options;
     Reflect.defineMetadata(PROTO_METADATA_KEY, existingFields, target);
   };
 }
 
-export function ProtoContract(options: ProtoContractOptions = {}) {
+export function ProtoContract(options: ProtoContractOptions = {}): ClassDecorator {
   return function(constructor: Function) {
     Reflect.defineMetadata(PROTO_CONTRACT_KEY, options, constructor);
   };
